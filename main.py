4d8ec987080d9cdaa9a473c674788b397a025f39
Bensas <juanbensa@gmail.com>
2020-11-13 13:43:52 -0300

    Added fonts and font parser

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from fonts import font1, font2, font3
+
+def parse_font(font):
+    result = np.array([])
+    for character in font:
+        processed_char = np.array([])
+        for column in character:
+            # This gets the binary string representation of the number (which starts with the chars 0b) and removed the first two chars, completes with zeroes
+            bits = bin(column)[2:].zfill(5)
+            for bit in bits:
+                processed_char = np.append(processed_char, int(bit))
+        result = np.append(result, processed_char)
+    result = result.reshape(32, 35)
+    return result
 
 command = input("Select the desired excercise:")
 
 if command == "1a":
   print("Loading data...")
-  raw_data = pd.read_csv("europe.csv")
-  country_names = raw_data["Country"]
-  raw_data = raw_data.drop(columns="Country")
-  data_normalized = (raw_data - raw_data.mean()) / raw_data.std()
-  data = data_normalized.to_numpy()
+  font = parse_font(font1)
 
   print("Exiting.")
 
