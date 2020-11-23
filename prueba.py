b9029c4359912a1e060926ef94d9e9ef4505d6a2
Yatex <foliver@gmail.com>
2020-11-23 12:10:34 -0300

    arreglos en hex to binary

--- prueba.py
+++ prueba.py
@@ -1,4 +1,4 @@
-fonts = [0x00,0x01,0x02,0x1a,0x01f]
+fonts = [0x00, 0x0e, 0x01, 0x0d, 0x13, 0x13, 0x0d]
 answer = [0] * len(fonts) * 5
 index_answer = 0
 index_array = 0
@@ -7,11 +7,14 @@ while(index_answer < len(fonts)):
     array = integer[2:]
     if(len(array) < 5):
         while(index_array < (5 - len(array))):
-            answer[index_answer*5 + index_array] = '0'
+            answer[index_answer*5 + index_array] = 0
             index_array = index_array + 1
     while(index_array < 5):
-        answer[(index_answer*5) + index_array] = array[index_array - (5 - len(array))]
+        answer[(index_answer*5) + index_array] = int(array[index_array - (5 - len(array))])
         index_array = index_array + 1
     index_answer = index_answer + 1
     index_array = 0
-return answer
+index_answer = 0
+while(index_answer < len(answer)):
+    print(answer[index_answer:index_answer+5])
+    index_answer = index_answer + 5
